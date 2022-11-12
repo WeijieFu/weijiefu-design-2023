cd2234f09e6e7c56c1293e9f7e3e262d96c281ae
WeijieFu <20438157+WeijieFu@users.noreply.github.com>
2022-11-12 22:18:22 +0000

    add eslint rules ignore

--- next.config.js
+++ next.config.js
@@ -6,6 +6,11 @@ const nextConfig = {
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
