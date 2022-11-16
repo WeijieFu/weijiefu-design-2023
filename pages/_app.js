3ea951fff87eefc1b41ca2d35216dd6667cef3eb
WeijieFu <20438157+WeijieFu@users.noreply.github.com>
2022-11-16 22:46:07 +0000

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import "../styles/globals.css"
 import "../constants/icons.js"
-
+import { GoogleAnalytics } from "nextjs-google-analytics"
 import { useEffect } from "react"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GoogleAnalytics trackPageViews />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
